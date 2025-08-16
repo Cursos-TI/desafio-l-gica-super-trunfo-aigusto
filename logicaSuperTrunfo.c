c59f4760edb41092319a1736ad8764cf47748a1e
aigusto <56740933+aigusto@users.noreply.github.com>
2025-08-16 16:44:50 -0300

    Update logicaSuperTrunfo.c

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,11 +6,7 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-
-    
-    pintf("Desafio Lógico do Super Trunfo")
-
-        
+       
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
 
